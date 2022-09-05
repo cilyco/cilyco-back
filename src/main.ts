2e84336b10648e8d91c75e41ffe7c012ed31463d
rmingon <ronanmingon69550@gmail.com>
2022-09-05 06:42:40 +0200

    add swagger conf

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+  .setTitle('Cilyco')
+  .setDescription('Cilyco API description')
+  .setVersion('1.0')
+  .addTag('cilyco')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
